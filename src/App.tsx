fda0e521ea531815a51fd70edb99407a6810ec21
OllyDS <desousa88@gmail.com>
2019-08-05 23:00:19 +0100

    preparing to create PersonalProjects page.

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import Home from "./components/Home";
 import CV from "./components/CV";
 import Contact from "./components/Contact";
 import OctopusWealthProject from "./components/molecules/OctopusWealthProject";
+import PersonalProjects from "./components/molecules/PersonalProjects";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="app-container">
-    <Navbar />
-    <Switch>
-      <Route exact={true} path="/" component={Home}/>
-      <Route exact={true} path="/cv/" component={CV} />
-      <Route exact={true} path="/contact/" component={Contact} />
-      <Route exact={true} path="/projects/octopus-wealth/" component={OctopusWealthProject} />
-      {/* <Route exact={true} path="/projects/personal-projects/" component={SomeRandomComponent} /> */}
-    </Switch>
+    <div className="app-container" id="top">
+      <Navbar />
+      <Switch>
+        <Route exact={true} path="/" component={Home}/>
+        <Route exact={true} path="/cv/" component={CV} />
+        <Route exact={true} path="/contact/" component={Contact} />
+        <Route exact={true} path="/projects/octopus-wealth/" component={OctopusWealthProject} />
+        <Route exact={true} path="/projects/personal-projects/" component={PersonalProjects} />
+      </Switch>
     </div>
   );
 };
