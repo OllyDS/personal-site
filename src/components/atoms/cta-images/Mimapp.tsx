f8b59cca0b0a36b801aa0a2d06b4111b9d5f7c23
OllyDS <desousa88@gmail.com>
2019-08-05 22:57:38 +0100

    Update the project components to use my own custom dimmer effect (futher reducing use of SemanticUI and using my own css). Also added new content for mobile view which looks far more user friendly.

--- src/components/atoms/cta-images/Mimapp.tsx
+++ src/components/atoms/cta-images/Mimapp.tsx
@@ -1,31 +1,100 @@
 import * as React from "react";
-import { Link } from "react-router-dom"
-import { Dimmer, Image, Header } from "semantic-ui-react";
+import styled, { css } from "styled-components";
+import { HashLink as Link } from "react-router-hash-link";
 
-const Mimapp = () => {
-  const [active, setActive] = React.useState(false);
+import { Cell, Container, Grid } from "../Grid";
 
-  const content = (
-    <div>
-      <Link to="/">
-        <Header as="h2" inverted>mimapp</Header>
-      </Link>
-    </div>
-  );
+const SImageContainer = styled.div`
+  display: none;
+  margin: auto;
+`;
 
-  return (
-    <Image.Group>
-      <Dimmer.Dimmable
-        as={Image}
-        src="/images/mimapp.png"
-        alt="image of a map titled mimapp"
-        dimmed={active}
-        dimmer={{ active, content }}
-        onMouseEnter={() => setActive(true)}
-        onMouseLeave={() => setActive(false)}
-      />
-    </Image.Group>
-  );
-};
+const SContainer = styled.div`
+  background-image: url(/images/mimapp.png);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  height: 450px;
+  margin: auto;
+  position: relative;
+  width: 900px;
+
+  @media (max-width: 639px) {
+    display: none
+  }
+
+  &:hover ${SImageContainer} {
+    background-color: #000000;
+    display: flex;
+    height: 100%;
+    margin: auto;
+    opacity: 0.8;
+    position: absolute;
+    width: 100%;
+
+    @media (max-width: 639px) {
+      display: none
+    }
+  }
+`;
+
+const textStyling = css`
+  align-items: center;
+  color: #FFFFFF;
+  display: flex;
+  font-size: 50px;
+  margin: auto;
+  position: relative;
+  text-align: center;
+`;
+
+const SCell = styled(Cell)`
+  display: flex;
+`;
+
+const SText = styled.h1`
+  ${textStyling}
+`;
+
+const SMobileImage = styled.img`
+  height: 100px
+  width: 100px;
+
+  @media(min-width: 640px) {
+    display: none;
+  }
+`;
+
+const SMobileText = styled.h1`
+  ${textStyling}
+  color: #1B1C1D;
+  display: flex;
+  font-size: 32px;
+  justify-content: center
+  @media (min-width: 640px) {
+    display: none;
+  }
+`;
+
+const Mimapp = () => (
+  <Link to="/projects/personal-projects/#mimapp">
+    <SContainer>
+      <SImageContainer>
+        <SText className="text">mimapp</SText>
+      </SImageContainer>
+    </SContainer>
+    <Container>
+      <Grid>
+        <SCell width={5}>
+          <SMobileImage src="/images/icons/map.png" />
+        </SCell>
+        <SCell width={7}>
+          <SMobileText>mimapp</SMobileText>
+        </SCell>
+      </Grid>
+    </Container>
+  </Link>
+);
 
 export default Mimapp;
